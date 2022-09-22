b7d22d82b7f5904124324ff1faaa611d3da33984
GabrielSilper <2019003726@ifam.edu.br>
2022-09-22 13:36:50 -0400

    terminei o exercicios de salário líquido - seção 4 dia 1

--- fundamentos/bloco-4-introducao-a-javascript/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/salario-liquido.js
+++ fundamentos/bloco-4-introducao-a-javascript/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/salario-liquido.js
@@ -1,34 +1,48 @@
 salarioBruto = 3000;
 let inss;
 let impostoRenda;
+let salarioDescontado;
+let salarioLiquido;
+
+console.log('Seu salário bruto é de: ',salarioBruto);
 
 //calculando o inss
 if (salarioBruto<=1556.94) {
     inss=salarioBruto*0.08;
-    console.log(inss);
+    console.log('Você paga '+inss+' de INSS.');
 }else if (salarioBruto>1556.94 && salarioBruto<=2594.92) {
     inss=salarioBruto*0.09;
-    console.log(inss);
+    console.log('Você paga '+inss+' de INSS.');
 }else if (salarioBruto>2594.93 && salarioBruto<=5189.82) {
     inss=salarioBruto*0.11;
-    console.log(inss);
+    console.log('Você paga '+inss+' de INSS.');
 }else if (salarioBruto>5189.82) {
     inss=570.88;
-    console.log(inss);
+    console.log('Você paga '+inss+' de INSS.');
 }else{
     console.log(false);
 }
 
 //tirando o inss do valor bruto
-salarioBruto-=inss;
-console.log(salarioBruto);
+salarioDescontado=salarioBruto-inss;
 
 //calculando o Imposto de Renda
-if (salarioBruto<=1903.98) {
+if (salarioDescontado<=1903.98) {
     console.log("Isento de imposto de renda");
-}else if (salarioBruto>1903.98 && salarioBruto<=2826.65) {
-}else if (salarioBruto>2826.65 && salarioBruto<=3751.05) {
-}else if (salarioBruto>5189.82) {
+}else if (salarioDescontado>1903.98 && salarioDescontado<=2826.65) {
+    impostoRenda= (salarioDescontado*0.075)-142.80;
+}else if (salarioDescontado>2826.65 && salarioDescontado<=3751.05) {
+    impostoRenda= (salarioDescontado*0.15)-354.80;
+}else if (salarioDescontado>3751.05 && salarioDescontado<=4664.68) {
+    impostoRenda= (salarioDescontado*0.225)-636.13;
+}else if (salarioDescontado>4664.68) {
+    impostoRenda= (salarioDescontado*0.275)-869.36;
 }else{
     console.log(false);
 }
+
+salarioLiquido=salarioDescontado-impostoRenda;
+console.log('Seu salário descontado INSS é de: ',salarioDescontado);
+console.log('Você paga '+impostoRenda+' de imposto de renda.');
+console.log('Seu salário líquido é de: ',salarioLiquido);
+console.log(salarioLiquido-(salarioBruto*0.17));
