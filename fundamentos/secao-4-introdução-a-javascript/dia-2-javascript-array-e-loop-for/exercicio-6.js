c490cf9b175b934a2b9e38068a8e9dfce2fdc38b
GabrielSilper <2019003726@ifam.edu.br>
2022-09-25 01:24:35 -0400

    exercicio 6 ok

---
+++ fundamentos/secao-4-introdução-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-6.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let cont = 0;
+
+for (let element of numbers) {
+    if(element%2!==0){
+        cont++;
+    }
+}
+
+console.log('A quantidade de números ímpares é: '+cont);
