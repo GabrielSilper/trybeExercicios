1d2431a79b561d669083681047a64aedadf40da1
GabrielSilper <2019003726@ifam.edu.br>
2022-09-25 01:29:00 -0400

    exercicio 7 ok

---
+++ fundamentos/secao-4-introdução-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-7.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smaller = numbers[0];
+
+for (let index = 1; index < numbers.length; index++) {
+    if (numbers[index]<smaller) {
+        smaller = numbers[index];
+    }
+}
+
+console.log('O menor número do Array numbers é: '+smaller);
