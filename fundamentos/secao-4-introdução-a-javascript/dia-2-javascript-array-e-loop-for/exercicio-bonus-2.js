42e249f47f0f1aab1509b59b55619d93674c90eb
GabrielSilper <2019003726@ifam.edu.br>
2022-09-25 02:22:27 -0400

    Bonus 2 - ok

---
+++ fundamentos/secao-4-introdução-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-bonus-2.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index++) {   
+    for (let i = 0; i < numbers.length-1; i++) {
+        if(numbers[i]<numbers[i+1]){
+            aux = numbers[i];
+            numbers[i]=numbers[i+1];
+            numbers[i+1]=aux;
+        }
+    }
+}
+
+console.log(numbers)
