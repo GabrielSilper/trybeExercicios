2eb6473d07e7d87e4a08034ccc2d18c3395969ec
GabrielSilper <2019003726@ifam.edu.br>
2022-09-30 19:29:03 -0400

    Parte 3 do Exercicio - ok

--- fundamentos/secao-5-JavaScript: DOM, Eventos e Web Storage/dia-3-JavaScript - Eventos/Exercicio do dia/script.js
+++ fundamentos/secao-5-JavaScript: DOM, Eventos e Web Storage/dia-3-JavaScript - Eventos/Exercicio do dia/script.js
@@ -21,6 +21,8 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
+// Escreva seu código abaixo.
+
   const decemberDaysList = [
     29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
@@ -46,4 +48,29 @@ createDaysOfTheWeek();
     }
   }
 
-// Escreva seu código abaixo.
+  function makeBtnHoliday(feriado) {
+    let btnHoliday = document.createElement('button');
+    btnHoliday.id = 'btn-holiday';
+    btnHoliday.innerHTML = feriado;
+    let container = document.querySelector('.buttons-container');
+    container.appendChild(btnHoliday);
+  }
+
+  makeBtnHoliday('Feriados');
+
+  function changeColorHoliday() {
+    let diasFeriados = document.querySelectorAll('.holiday');
+    for (let elemento of diasFeriados) {
+        if(elemento.style.backgroundColor !== 'lightgreen'){
+            elemento.style.backgroundColor = 'lightgreen'
+        }else if (elemento.style.backgroundColor === 'lightgreen'){
+            elemento.style.backgroundColor = 'rgb(238,238,238)'
+        }
+    }
+  }
+
+  let btnFeriados = document.querySelector('#btn-holiday');
+  btnFeriados.addEventListener('click',changeColorHoliday);
+
+
+
