e04f896e36a54fa29d56ff678118a11ff7680f6b
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-12 18:10:31 -0400

    Inicio do Exercicio do dia

---
+++ frontend/secao-1/dia-2/Exercicio-dia/src/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+const btnSortear = document.querySelector('button');
+const heroName = document.querySelector('h3');
+const heroImg = document.querySelector('img');
+
+
+
+// const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/90.json');
+// const data = await response.json();
+
+console.log(data);
