50d70b4600e96ce013ea3cf22e03c19d07511a85
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-13 04:54:57 -0400

    Exercício Principal feito

--- frontend/secao-1/dia-2/Exercicio-dia/src/main.js
+++ frontend/secao-1/dia-2/Exercicio-dia/src/main.js
@@ -1,12 +1,32 @@
-import './style.css';
+import "./style.css";
 
-const btnSortear = document.querySelector('button');
-const heroName = document.querySelector('h3');
-const heroImg = document.querySelector('img');
+const btnSortear = document.querySelector("button");
+const heroName = document.querySelector("h3");
+const heroImg = document.querySelector("img");
 
+const randomID = () => {
+  const TAM = 751;
+  return Math.floor(Math.random() * TAM);
+};
 
+const randomHero = async () => {
+  const N = randomID();
+  const URL_API = `https://akabab.github.io/superhero-api/api/id/${N}.json`;
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  return data;
+};
 
-// const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/90.json');
-// const data = await response.json();
-
-console.log(data);
+btnSortear.addEventListener("click", async () => {
+  try {
+    const data = await randomHero();
+    heroImg.src = data.images.sm;
+    setTimeout(() => {
+      heroName.innerText = data.name;
+    }, 200);
+  } catch (error) {
+    heroImg.src = "./images/blank.jpg";
+    heroName.innerHTML = "Error: ID sorteado não existe!";
+  }
+  // heroName.innerHTML = JSON.stringify(data);
+});
